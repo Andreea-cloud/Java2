0915c252e705b98f1c7c70cd98e94d1101198640
Andreea Nicut <andreea.nicut30@gmail.com>
2021-02-13 09:32:09 +0200

    persistence

---
+++ library/src/main/java/library/service/UserService.java
@@ -0,0 +1,12 @@
+package library.service;
+
+import library.dto.UserDTO;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface UserService extends Remote {
+    int login(UserDTO userDTO) throws RemoteException;
+
+    int signin(UserDTO userDTO) throws RemoteException;
+}
