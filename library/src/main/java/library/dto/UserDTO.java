dc27453039e8345c0f13d13624913513c01c9f97
Andreea Nicut <andreea.nicut30@gmail.com>
2021-02-13 09:13:06 +0200

    initial commit

---
+++ library/src/main/java/library/dto/UserDTO.java
@@ -0,0 +1,50 @@
+package library.dto;
+
+import java.util.Objects;
+
+public class UserDTO {
+    private int id;
+    private String username;
+    private String password;
+
+    public UserDTO(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    UserDTO(){
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDTO userDTO = (UserDTO) o;
+        return id == userDTO.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
